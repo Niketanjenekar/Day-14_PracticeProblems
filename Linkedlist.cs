df19de6108edd076f1d97fa3b693c915d58deb1d
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-29 23:54:33 +0530

    Create a stack and pushing data

---
+++ Linkedlist.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Day14_PracticeProblems
+{
+    public class Linkedlist
+    {
+        public Node top;
+
+        public Linkedlist()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Day14_PracticeProblems
+﻿using System.Collections.Generic;
+
+namespace Day14_PracticeProblems
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack and Queue Problems: ");
+
+            Linkedlist linkedlist = new Linkedlist();
+            linkedlist.Push(56);
+            linkedlist.Push(30);
+            linkedlist.Push(70);
+
+            linkedlist.Display();
         }
     }
 }
